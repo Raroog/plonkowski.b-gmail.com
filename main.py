9c85b2316b1eff990a5a448d94ba8d0b9135f1f7
Raroog <plonkowski.b@gmail.com>
2020-04-05 19:18:30 +0200

    he

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 #main.py
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(debug = 'true')
 
-@app.get("/")
-def root():
-    return{"message": "hello world"}
+#class Prowadzacyssiepaue(BaseModel):
+#	msg :str
+
+@app.get("/method")
+async def get_fun():
+    return {"get": "GET"}
+
+
+#@app.post("/method")
+#async def post_fun():
+#	return{"post" : "POST"}
+
+
+#@app.put("/method")
+#async def put_fun():
+#	return {"put": "PUT"}
+
+
+#@app.delete("/method")
+#async def delete_fun():
+#	return{"delete" : "DELETE"}   
