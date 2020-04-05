1495deb1f2837771161d61d9651b64af95278cc3
Bartłomiej Płonkowski <43927837+Raroog@users.noreply.github.com>
2020-04-05 19:25:45 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ from pydantic import BaseModel
 
 app = FastAPI(debug = 'true')
 
+
+#class Prowadzacyssiepaue(BaseModel):
+#	msg :str
+
 @app.get("/method")
 async def get_fun():
     return {"get": "GET"}
 
-
-@app.post("/method")
-async def post_fun():
-	return{"post" : "POST"}
-
-
-@app.put("/method")
-async def put_fun():
-	return {"put": "PUT"}
+#@app.post("/method")
+#async def post_fun():
+#	return{"post" : "POST"}
 
 
-@app.delete("/method")
-async def delete_fun():
-	return{"delete" : "DELETE"}   
+#@app.put("/method")
+#async def put_fun():
+#	return {"put": "PUT"}
+
+
+#@app.delete("/method")
+#async def delete_fun():
+#	return{"delete" : "DELETE"}   
+
