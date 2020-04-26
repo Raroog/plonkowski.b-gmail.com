d09a1dcb8d3b7662834ea69925f50bdf1578e81c
Raroog <plonkowski.b@gmail.com>
2020-04-26 15:39:41 +0200

    nowe

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 #main.py
 
-
+from hashlib import sha256
+from fastapi import FastAPI, Response, Cookie, HTTPException
 from fastapi import FastAPI, Request, Response, status
-
+import secrets
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 
+security = HTTPBasic()
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "trudnY")
+    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+	if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return 
 
 
-@app.get("/welcome")
-def read_root():
-    return {"message": "Welcome universe, this is planet Earth"}
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello men and women"}
+
+@app.post('/login')
+def read_current_user(username: str = Depends(get_current_username)):
+    return RedirectResponse(url="/welcome", status_code=HTTP_302_FOUND)
+
+
+
+
 
 
 
